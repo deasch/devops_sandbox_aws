db11cf8747e9c5bf09fdf9c819aabf0c601bba66
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 00:59:23 +0200

    Update install_jenkins.sh

--- github-actions_terraform_jenkins/modules/compute/install_jenkins.sh
+++ github-actions_terraform_jenkins/modules/compute/install_jenkins.sh
@@ -4,4 +4,11 @@ sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sour
 sudo apt update && sudo apt upgrade -y
 sudo apt install default-jre -y
 sudo apt install jenkins -y
+
+
+ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
+# https://abrahamntd.medium.com/automating-jenkins-setup-using-docker-and-jenkins-configuration-as-code-897e6640af9d
+#ENV CASC_JENKINS_CONFIG /var/jenkins_home/jenkins-configuration.yaml
+#jenkins-plugin-cli --plugin-file /usr/share/jenkins/ref/plugins.txt
+
 sudo systemctl start jenkins
