dc49a2b88cf38ab5f078a0f4dcd364662ee87f9c
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-17 21:27:58 +0200

    Update main.tf

--- github-actions_terraform_jenkins_docker/main.tf
+++ github-actions_terraform_jenkins_docker/main.tf
@@ -1,3 +1,29 @@
+variable "aws_region" {
+   default = "us-east-2"
+}
+
+variable "vpc_cidr_block" {
+   description = "CIDR block for VPC"
+   type = string
+   default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_block" {
+   description = "CIDR block for public subnet"
+   type = string
+   default = "10.0.1.0/24"
+}
+
+variable "my_ip" {
+   description = "Your IP address"
+   type = string
+   sensitive = true
+}
+
+
+
+
+
 terraform {
   required_providers {
     aws = {
@@ -5,6 +31,11 @@ terraform {
       version = "~> 4.0"
     }
   }
+}
+
+provider "aws" {
+   region = var.aws_region
+   profile = "default"
 }
 
 module "vpc" {
@@ -19,4 +50,13 @@ module "ec2_instance" {
   public_subnet  = module.vpc.public_subnet_id
   vpc_id         = module.vpc.vpc_id
   my_ip          = var.my_ip
+}
+
+
+
+
+
+output "jenkins_public_ip" {
+   description = "The public IP address of the Jenkins server"
+   value = module.ec2_instance.public_ip
 }
