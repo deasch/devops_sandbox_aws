9dca19a85fa0636c739024ba932c81025363e1f3
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 01:38:35 +0200

    Update main.tf

--- github-actions_terraform_jenkins/modules/compute/main.tf
+++ github-actions_terraform_jenkins/modules/compute/main.tf
@@ -46,6 +46,10 @@ resource "aws_instance" "jenkins_server" {
    tags = {
       Name = "jenkins_server"
    }
+
+   lifecycle {
+       create_before_destroy = true
+     }
 }
 
 #resource "aws_key_pair" "aws_kp" {
