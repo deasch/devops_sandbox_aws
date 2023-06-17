cc7e27824f26a64f430001f6ddf9b8adcb64d72b
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-17 20:52:47 +0200

    Create outputs.tf

---
+++ github-actions_terraform_jenkins_aws-ec2/outputs.tf
@@ -0,0 +1,9 @@
+output "jenkins_public_ip" {
+   description = "The public IP address of the Jenkins server"
+   value = module.ec2_instance.public_ip
+}
+  
+  output "jenkins_public_ip" {
+   description = "The public IP address of the Jenkins server"
+   value = module.ec2_instance.public_dns
+}
