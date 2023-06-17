839da30bc8c096d063afc17d4d57dc34c2c24830
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 01:56:27 +0200

    Update main.tf

--- github-actions_terraform_jenkins/modules/vpc/main.tf
+++ github-actions_terraform_jenkins/modules/vpc/main.tf
@@ -18,10 +18,6 @@ resource "aws_vpc" "aws_vpc" {
 	cidr_block = var.vpc_cidr_block
 	enable_dns_hostnames = true
 
-	lifecycle {
-		create_before_destroy = true
-	}
-
 	tags = {
 		Name = "aws_vpc"
 	}
@@ -29,10 +25,6 @@ resource "aws_vpc" "aws_vpc" {
 
 resource "aws_internet_gateway" "aws_igw" {
 	vpc_id = aws_vpc.aws_vpc.id
-
-	lifecycle {
-		create_before_destroy = true
-	}
 
 	tags = {
 		Name = "aws_igw"
@@ -44,10 +36,6 @@ resource "aws_subnet" "aws_public_subnet" {
 	cidr_block = var.public_subnet_cidr_block
 	availability_zone = data.aws_availability_zones.available.names[0]
 
-	lifecycle {
-		create_before_destroy = true
-	}
-
 	tags = {
 		Name = "aws_public_subnet_1"
 	}
@@ -55,10 +43,6 @@ resource "aws_subnet" "aws_public_subnet" {
 
 resource "aws_route_table" "aws_public_rt" {
 	vpc_id = aws_vpc.aws_vpc.id
-
-	lifecycle {
-		create_before_destroy = true
-	}
 
 	route {
 		cidr_block = "0.0.0.0/0"
@@ -69,10 +53,6 @@ resource "aws_route_table" "aws_public_rt" {
 resource "aws_route_table_association" "public" {
 	route_table_id = aws_route_table.aws_public_rt.id
 	subnet_id = aws_subnet.aws_public_subnet.id
-
-	lifecycle {
-		create_before_destroy = true
-	}
 }
 
 
