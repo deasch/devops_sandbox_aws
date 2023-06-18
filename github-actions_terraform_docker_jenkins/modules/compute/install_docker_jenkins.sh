87eaa40bd598c349a02317c43bc26237c06b57e1
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 10:28:53 +0200

    Update install_docker_jenkins.sh

--- github-actions_terraform_docker_jenkins/modules/compute/install_docker_jenkins.sh
+++ github-actions_terraform_docker_jenkins/modules/compute/install_docker_jenkins.sh
@@ -17,7 +17,7 @@ sudo docker info
 
 #Install Docker Compose
 #sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-'uname -s' - 'uname -m' | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl restart docker
 sudo docker-compose --version
@@ -28,61 +28,22 @@ sudo mkdir -p /home/ec2-user/jenkins_data/jenkins_home
 
 
 #Create Docker Compose File
-
-
-
-
-sudo cd /home/ec2-user/jenkins_data
 sudo bash -c 'cat << EOF > /home/ec2-user/jenkins_data/docker-compose.yml
 version: "3"
 
 services:
   jenkins:
     container_name: jenkins
-    image: jenkins/jenkins:lts-jdk11
-    restart: unless-stopped
+    image: jenkins/jenkins
     ports:
       - "8080:8080"
     volumes:
-      - "/home/ec2-user/jenkins_data/jenkins_home:/var/jenkins_home"
+      - "./jenkins_home:/var/jenkins_home"
 EOF'
 
 
 #Run the Docker Compose file
+cd /home/ec2-user/jenkins_data/
 sudo docker-compose up -d
 
 
-
-
-
-sudo cat > docker-compose.yml <<- "EOF"
-version: '3'
-
-services:
-  jenkins:
-    container_name: jenkins
-    image: jenkins/jenkins:lts-jdk11
-    restart: unless-stopped
-    ports:
-      - "8080:8080"
-    volumes:
-      - "/home/ec2-user/jenkins_data/jenkins_home:/var/jenkins_home"
-EOF
-
-
-
-
-
-
-
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo dnf install java-11-amazon-corretto -y
-sudo yum install jenkins -y
-
-JAVA_OPTS='-Djenkins.install.runSetupWizard=false'
-
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
