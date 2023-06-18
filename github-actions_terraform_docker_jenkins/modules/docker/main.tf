747839b4519e210ddac99c6181a304dabe7306e0
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 12:05:43 +0200

    Update main.tf

--- github-actions_terraform_docker_jenkins/modules/docker/main.tf
+++ github-actions_terraform_docker_jenkins/modules/docker/main.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+
+
 
