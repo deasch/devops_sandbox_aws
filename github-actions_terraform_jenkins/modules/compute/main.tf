1a80de16caa42b35e793da10c41cf62c9035fbef
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 02:01:47 +0200

    Update main.tf

--- github-actions_terraform_jenkins/modules/compute/main.tf
+++ github-actions_terraform_jenkins/modules/compute/main.tf
@@ -55,7 +55,7 @@ resource "aws_instance" "jenkins_server" {
 #}
 
 resource "aws_eip" "jenkins_eip" {
-   instance = aws_instance.jenkins_server.id[0]
+   instance = aws_instance.jenkins_server[1].id
    vpc      = true
 
    tags = {
