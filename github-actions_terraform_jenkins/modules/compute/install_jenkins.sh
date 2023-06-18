997d1d53a5815b53fdcc0365fa63b7172ad88e6d
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 02:13:58 +0200

    Update install_jenkins.sh

--- github-actions_terraform_jenkins/modules/compute/install_jenkins.sh
+++ github-actions_terraform_jenkins/modules/compute/install_jenkins.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update && sudo apt upgrade -y
-sudo apt install default-jre -y
-sudo apt install jenkins -y
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-11-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
 sudo systemctl start jenkins
+
+
+
+
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+#sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo apt update && sudo apt upgrade -y
+#sudo apt install default-jre -y
+#sudo apt install jenkins -y
+#sudo systemctl start jenkins
