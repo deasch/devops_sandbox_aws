05b34c04a1e24ac079d78b448d038b659b662f54
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 09:45:30 +0200

    Update install_docker_jenkins.sh

--- github-actions_terraform_docker_jenkins/modules/compute/install_docker_jenkins.sh
+++ github-actions_terraform_docker_jenkins/modules/compute/install_docker_jenkins.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+# https://medium.com/@raguyazhin/step-by-step-guide-to-install-jenkins-on-amazon-linux-bfce88dd5a9e
+
+
+#Install Docker
+sudo yum update –y
+sudo yum upgrade
+sudo dnf install docker -y
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -a -G docker ec2-user
+sudo chkconfig docker on
+sudo systemctl restart docker
+docker info
+
+
+
+
+
+
+
+
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
