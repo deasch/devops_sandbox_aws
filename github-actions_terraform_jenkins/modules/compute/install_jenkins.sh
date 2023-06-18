777d485e1f9f2fd825af5816659e372a648a7a54
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 03:03:10 +0200

    Update install_jenkins.sh

--- github-actions_terraform_jenkins/modules/compute/install_jenkins.sh
+++ github-actions_terraform_jenkins/modules/compute/install_jenkins.sh
@@ -5,15 +5,8 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo dnf install java-11-amazon-corretto -y
 sudo yum install jenkins -y
+
+JAVA_OPTS='-Djenkins.install.runSetupWizard=false'
+
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-
-
-
-
-#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-#sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-#sudo apt update && sudo apt upgrade -y
-#sudo apt install default-jre -y
-#sudo apt install jenkins -y
-#sudo systemctl start jenkins
